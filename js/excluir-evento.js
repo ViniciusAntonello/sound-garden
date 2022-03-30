ce69813e9af61e9e79d3c07511676125a91f157b
ViniciusAntonello <antonello.luis@gmail.com>
2022-03-30 00:36:51 -0300

    Third try on Excluir-Evento

--- js/excluir-evento.js
+++ js/excluir-evento.js
@@ -1,71 +1,43 @@
 const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
 
-const inputName = document.querySelector('#nome')
-const inputBanner = document.querySelector('#banner')
-const inputAttractions = document.querySelector('#atracoes')
-const inputDescription = document.querySelector('#descricao')
-const inputDate = document.querySelector('#data')
-const inputCapacity = document.querySelector('#lotacao')
+const inputName = document.querySelector('#nome');
+const inputBanner = document.querySelector('#banner');
+const inputAttractions = document.querySelector('#atracoes');
+const inputDescription = document.querySelector('#descricao');
+const inputDate = document.querySelector('#data');
+const inputCapacity = document.querySelector('#lotacao');
+const form = document.querySelector('form');
 
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id');
 
-// METÓDO 1 - Tomei por base o código da Tiaki
+const requestOption = {
+    method: 'GET',
+    redirect: 'follow'
+};
 
+const request = async () => {
+    const request = await fetch(`${BASE_URL}/events/${id}`)
+    const response = await request.json()
+    const {name, poster, attraction, description, schedule, number_tickets} = response
 
-// const loadEvent = async () => {
-//     const params = new URLSearchParams(window.location.search);
-//     const id = params.get("id");
-
-//     const event = await getEventById(id);
-//     const date = new Date(event.scheduled);
-
-//     inputName.value = event.name
-//     inputBanner.value = event.psoter
-//     inputAttractions.value = event.attractions
-//     inputDescription.value = event.description
-//     inputDate.value = date.toLocaleDateString('pt-BR') + ' '+ date.toLocaleTimeString('pt-BR')
-//     inputCapacity.value = event.number_tickets
-
-// }
-
-// async function getEventById(id) {
-//     const receive = {
-//         method: "GET",
-//         dataType: "Json",
-//         headers: {
-//             "Content-Type": "application/json"
-//         }
-//     };
-// }
-
-// loadEvent();
-
-
-// MÉTODO 2 - Um Frankstain de códigos e vídeos =/
-
-const urlParams = new URLSearchParams(window.location.search)
-const paramId = urlParams.get('id')
-
-window.onload = async () => {
-    const answer = await fetch(`${BASE_URL}/events/${paramId}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-
-    const inputedAnswer = await answer.json()
-
-    console.log(inputedAnswer);
-
-    inputName.value = inputedAnswer.name
-    inputBanner.value = inputedAnswer.psoter
-    inputAttractions.value = inputedAnswer.attractions
-    inputDescription.value = inputedAnswer.description
-    inputCapacity.value = inputedAnswer.number_tickets
-
-    // newDate = `${inputedAnswer.scheduled[8]}${inputedAnswer.scheduled[9]}/${inputedAnswer.scheduled[5]}${inputedAnswer.scheduled[6]}/${inputedAnswer.scheduled[0]}${inputedAnswer.scheduled[1]}${inputedAnswer.scheduled[2]}${inputedAnswer.scheduled[3]} ${inputedAnswer.scheduled[11]}${inputedAnswer.scheduled[12]}:${inputedAnswer.scheduled[14]}${inputedAnswer.scheduled[15]} `
-
-    // inputDate.value = newDate
-
-
+    inputName.value = name
+    inputBanner.value = poster
+    inputAttractions.value = attraction
+    inputDescription.value = description
+    inputDate.value = schedule
+    inputCapacity.value = number_tickets
 }
+
+request();
+
+form.addEventListener('submit', async event => {
+    event.preventDefault()
+    const requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+    }
+    const request = await fetch(`${BASE_URL}/events/${id}`, requestOption);
+    alert('Evento excluido!')
+    window.location.replace('./admin.html')
+})
